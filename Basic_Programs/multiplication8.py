ce956665df9b569f7c68920f5c8c29788faa46fe
Yash Andhale <mailtoyashandhale@gmail.com>
2025-11-18 21:49:02 +0530

    Add files via upload

---
+++ Basic_Programs/multiplication8.py
@@ -0,0 +1,17 @@
+import Arithmatic
+
+def main():
+    print("Enter first number:")
+    no1=int(input())
+    
+    print("Enter second number:")
+    no2=int(input())
+    
+    ans=Arithmatic.Multiplication(no1,no2)
+    
+    
+    print("Multiplication is :",ans)
+    
+if __name__ == "__main__":
+    main()  
+    
