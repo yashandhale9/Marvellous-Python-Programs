b0f2a7b142bf20699e26050c928ce50f8c00b8b5
Yash Andhale <mailtoyashandhale@gmail.com>
2025-11-18 21:48:37 +0530

    Add files via upload

---
+++ Basic_Programs/percentage1.py
@@ -0,0 +1,14 @@
+def main():
+    print("Enter total marks:")
+    Value1=int(input)
+    
+    print("Enter obtained marks:")
+    Value2=int(input)
+    
+    outcome=(Value2/Value1)*100
+    
+    print("Percentage is:",outcome)
+    
+if __name__ == "__main__":
+    main()
+    main()
